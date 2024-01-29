be4c59fb05c41c0ae6c457782c6cab80f66c2b9f
Alessandro Borraccino <alessandro.borraccino@fisglobal.com>
2024-01-29 18:15:30 +0000

    first commit

---
+++ src/test/java/com/worldpay/t24/graalvm/helloworld/HelloworldSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worldpay.t24.graalvm.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
